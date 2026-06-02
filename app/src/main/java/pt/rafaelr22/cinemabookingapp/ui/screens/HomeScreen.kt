fbb5a59dc06b673fc4290b1e6e5c2a3567b7f765
Rafael Ricardo <rafael.reis.ricardo@live.com.pt>
2026-06-02 19:44:26 +0100

    Implement movie cards on home screen

--- app/src/main/java/pt/rafaelr22/cinemabookingapp/ui/screens/HomeScreen.kt
+++ app/src/main/java/pt/rafaelr22/cinemabookingapp/ui/screens/HomeScreen.kt
@@ -1,18 +1,51 @@
 package pt.rafaelr22.cinemabookingapp.ui.screens
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import pt.rafaelr22.cinemabookingapp.data.model.Movie
+import pt.rafaelr22.cinemabookingapp.ui.components.MovieCard
 
 @Composable
 fun HomeScreen() {
+
+    val movies = listOf(
+        Movie(
+            id = 1,
+            title = "Dune: Part Two",
+            genre = "Sci-Fi",
+            duration = "166 min",
+            description = "Paul Atreides unites with the Fremen..."
+        ),
+        Movie(
+            id = 2,
+            title = "Deadpool & Wolverine",
+            genre = "Action",
+            duration = "127 min",
+            description = "Deadpool joins forces with Wolverine..."
+        ),
+        Movie(
+            id = 3,
+            title = "Oppenheimer",
+            genre = "Drama",
+            duration = "180 min",
+            description = "The story of J. Robert Oppenheimer..."
+        )
+    )
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(
+                top = 48.dp,
+                start = 16.dp,
+                end = 16.dp
+            )
     ) {
+
         Text(
             text = "Cinema Booking",
             style = MaterialTheme.typography.headlineMedium
@@ -20,8 +53,8 @@ fun HomeScreen() {
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text("Dune: Part Two")
-        Text("Deadpool & Wolverine")
-        Text("Oppenheimer")
+        movies.forEach { movie ->
+            MovieCard(movie = movie)
+        }
     }
 }
